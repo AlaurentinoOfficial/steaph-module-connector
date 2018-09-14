ffd5c181334a3a7d83a2e0a946cea77172c5d836
Anderson Laurentino <alaurentino.br@gmail.com>
2018-09-14 18:13:19 -0300

    Update the Solution Model

--- src/app/models/solution.js
+++ src/app/models/solution.js
@@ -3,8 +3,6 @@ var relationship = require("mongoose-relationship")
 
 let solutionSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},
-    uuid: {type: String, required: false},
-    token: {type: String, required: false},
     status: {type: Boolean, default: true, require: false},
     cpf_cnpj: {type: String, required: true},
     type: {type: String, enum: ["physical", "legal"], required: true},
