a08c936e304daf532dc9a26444c60fe2dd499f46
Anderson Laurentino <alaurentino.br@gmail.com>
2018-09-19 21:12:16 -0300

    Create a two ways to connect to the Steaph API -> Kafka and HTTP

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-import { SteaphAPI } from './helpers/http_request'
+import { SteaphKafka } from './services/steaph_api'
 import { GetListDevices } from './helpers/device_status'
 import { green, cyan, bold, yellow } from 'colors'
 import KNoTCloud from 'knot-cloud'
 
-const api = new SteaphAPI(
-    "127.0.0.1",
-    "8080",
-    "ahdskfjwoikfadsf03i4ohrje0989uh3owefaihojn32whiaeojdsfjaosdkf"
-)
+const api = new SteaphKafka("http://127.0.0.1:9022")
 
 const cloud = new KNoTCloud(
     'knot-test.cesar.org.br',
@@ -16,38 +12,32 @@ const cloud = new KNoTCloud(
     'd5265dbc4576a88f8654a8fc2c4d46a6d7b85574',
 );
 
-var loopCondition = true;
-async function Loop(delay) {
+var handlerCondition = true;
+async function Handler(delay) {
     var devicesStatus = []
 
+    // Get devices list
     try {
-        // Query in Steaph API
-        var environments = await api.getEnvironments()
-        var schedules = await api.getSchedules()
+        devicesStatus = GetListDevices(api.getEnvironments(), api.getSchedules())
+    } catch(err) {console.log(err)}
 
-        devicesStatus = GetListDevices(environments, schedules)
-    }
-    catch(err) {
-        console.log(err)
-    }
-
-    // Send to the devices
+    // Send to the devices their status
     for(var m of devicesStatus) {
         console.log(green('➜  ') + yellow('<' + m.id + '> ') + m.status)
-        await cloud.setData(m.id, [{ sensorId: 1, value: m.status }]);
+        //await cloud.setData(m.id, [{ sensorId: 1, value: m.status }]);
     }
 
     // Create a loop
-    if(loopCondition)
-        setTimeout(() => {Loop(delay)}, delay)
+    if(handlerCondition) setTimeout(() => {Handler(delay)}, delay)
 }
 
 async function main() {
-    await cloud.connect();
+    //await cloud.connect();
     console.log(green('➜  ') + bold(cyan('SERVER:')) + " Service is running!!")
 
-    await Loop(30000)
+    await Handler(30000)
 
-    await cloud.close();
+    api.close()
+    //await cloud.close();
 }
 main();
---
+++ src/services/steaph_api.js
@@ -0,0 +1,66 @@
+import axios from 'axios'
+import { KafkaClient, Consumer } from 'kafka-node'
+import { green, blue, bold } from 'colors'
+
+export class SteaphKafka {
+    constructor(host) {
+        this.environments = []
+        this.schedules = []
+
+        this.client = new KafkaClient({kafkaHost: `${this.host}`})
+        this.environmentConsumer = new Consumer(this.client, [{topic: "steaph.environments", partition: 0}], {autoCommit: false})
+        this.scheduleConsumer = new Consumer(this.client, [{topic: "steaph.schedules", partition: 0}], {autoCommit: false})
+    
+        this.environmentConsumer.on('message', (msg) => {
+            this.environments = msg
+        })
+
+        this.scheduleConsumer.on('message', (msg) => {
+            this.schedules = msg
+        })
+    }
+
+    getEnvironments() {
+        return this.environments
+    }
+
+    getSchedules() {
+        return this.schedules
+    }
+
+    close() {
+        this.environmentConsumer.close(null)
+        this.scheduleConsumer.close(null)
+        this.client.close(null)
+    }
+}
+
+export class SteaphHTTP {
+    constructor(host, port, token) {
+        this.host = host
+        this.port = port
+        this.token = token
+    }
+
+    async getEnvironments() {
+        try {
+            const res = await axios(`http://${this.host}${this.port ? ":" + this.port : ""}/local/environment`)
+            return res.data
+        } 
+        catch(err) {
+            console.log("Connection to API error!")
+            return []
+        }
+    }
+
+    async getSchedules() {
+        try {
+            const res = await axios(`http://${this.host}${this.port ? ":" + this.port : ""}/local/schedule`)
+            return res.data
+        } 
+        catch(err) {
+            console.log("Connection to API error!")
+            return []
+        }
+    }
+}
